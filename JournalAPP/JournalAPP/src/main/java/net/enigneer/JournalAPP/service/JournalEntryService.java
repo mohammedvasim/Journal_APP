ce44463f4789679118e55ebc1ff9f43666a05a66
Mohammed Vasim <vasimps30@gmial.com>
2025-11-30 12:51:19 +0530

    Added Transactional annotation

--- JournalAPP/JournalAPP/src/main/java/net/enigneer/JournalAPP/service/JournalEntryService.java
+++ JournalAPP/JournalAPP/src/main/java/net/enigneer/JournalAPP/service/JournalEntryService.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import javax.management.RuntimeErrorException;
+
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import net.enigneer.JournalAPP.entity.JournalEntry;
 import net.enigneer.JournalAPP.entity.User;
@@ -22,12 +25,22 @@ public class JournalEntryService {
     @Autowired
     private UserService Userservice;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry,String userName){
-        User user=Userservice.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved=JournalEntryrepo.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        Userservice.saveEntry(user);
+
+        try{
+                User user=Userservice.findByUserName(userName);
+                journalEntry.setDate(LocalDateTime.now());
+                JournalEntry saved=JournalEntryrepo.save(journalEntry);
+                user.getJournalEntries().add(saved);
+                user.setUsername(null);
+                Userservice.saveEntry(user);
+        }
+        catch(Exception e){
+            System.out.println(e);
+            throw new RuntimeException("an error occured while saving the entry",e);
+        }
+
     }
 
     public void saveEntry(JournalEntry journalEntry){
